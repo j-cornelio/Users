3126a4e648bfb2d77449a98b336a06f1a6ae2e41
julio <empjulio@hotmail.com>
2018-06-07 14:03:22 -0400

    filtering

--- src/components/Users/UsersTable.js
+++ src/components/Users/UsersTable.js
@@ -70,15 +70,43 @@ const SortOptions = ({ alpha, defaultFunc, priority }) => (
 	</div>
 )
 
+
+		 // <input type="radio" id="contactChoice1"
+		 //     name="contact" value="email" />
+		 //    <label for="contactChoice1">Email</label>
+
+const FilterOptions = ({ users, handleFilter }) => {
+	console.log('form: ', users);
+	const unique =  users.map(el => el.category)
+						 .filter((el, idx, array) => array.indexOf(el) === idx)
+	console.log(unique)
+	return (
+		<div>	
+			<p>Filter by Priority</p>				 	
+			<form>
+				{
+					 unique.map( elem => (
+					 	<span key={elem}>
+					 		<input type="radio" id="contactChoice1" name="priority" value={elem} /> 
+						    <label>{elem}</label>
+					    </span>
+					 ))
+				}
+			</form>
+		</div>
+	)
+};//
+
 class UsersTable extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
 			users: USERS.data
 		}
-  		this.alpha = this.alpha.bind(this);
-  		this.priority = this.priority.bind(this);
-  		this.default = this.default.bind(this);
+  		this.alpha 			= this.alpha.bind(this);
+  		this.priority 		= this.priority.bind(this);
+  		this.default 		= this.default.bind(this);
+  		this.handleFilter 	= this.handleFilter.bind(this);
 	}
 
 	default(){
@@ -99,11 +127,16 @@ class UsersTable extends Component {
 		this.setState((prevState) => ({users: sorted}));
 	}
 
+	handleFilter(){
+		console.log('filter')
+	}
+
 	render() {
 		const { users } = this.state;
-		console.log(users)
+
 		return (
 			<div>
+				<FilterOptions users={users} />
 				<SortOptions alpha={this.alpha} defaultFunc={this.default} priority={this.priority} />
 				<Table users={users} />
 			</div>
